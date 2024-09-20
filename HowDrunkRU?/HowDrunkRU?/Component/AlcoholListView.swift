3a5b2bc828e2df93def459e1e1eb565ae07a70d6
loic-rouzaud <loic.rouzaud@epitech.eu>
2024-09-20 16:37:47 +0200

    [ADD] - list changements

--- HowDrunkRU?/HowDrunkRU?/Component/AlcoholListView.swift
+++ HowDrunkRU?/HowDrunkRU?/Component/AlcoholListView.swift
@@ -10,27 +10,47 @@ import SwiftUI
 struct AlcoholListView: View {
     @EnvironmentObject var dataManager: DataManager
     @State private var showAlcoholSheet = false
-    
+    @State private var showAddButton = false
+
     var body: some View {
         NavigationView {
-            List(dataManager.alcohol, id: \.id) { alcohol in
-                Text(alcohol.name + " " + "\(alcohol.percentage)%" + " " + alcohol.type)
+            VStack {
+                alcoholList
+                    .navigationTitle("Alcohol List")
+                    .navigationBarItems(trailing: showAddButton ? addButton : nil)
+                    .onAppear {
+                        updateAddButtonVisibility()
+                    }
             }
-            .navigationTitle("Alcohol List")
-            .navigationBarItems(trailing: Button(action: {
-                showAlcoholSheet.toggle()
-            }, label: {
-                Image(systemName: "plus")
-            }))
             .sheet(isPresented: $showAlcoholSheet) {
                 NewAlcoholView()
                     .presentationDetents([.medium, .large])
             }
         }
     }
+
+    private var alcoholList: some View {
+        List(dataManager.alcohol, id: \.id) { alcohol in
+            Text(alcohol.name + " " + "\(alcohol.percentage)%" + " " + alcohol.type)
+        }
+    }
+
+    private var addButton: some View {
+        Button(action: {
+            showAlcoholSheet.toggle()
+        }, label: {
+            Image(systemName: "plus")
+        })
+    }
+
+    private func updateAddButtonVisibility() {
+        let screenHeight = UIScreen.main.bounds.height
+        let listHeight = 200.0
+
+        print("SCREEN HEIGHt : ", screenHeight)
+        showAddButton = listHeight > screenHeight / 2
+    }
 }
-
-
 #Preview {
     AlcoholListView()
         .environmentObject(DataManager())
